60b4b8d05a3239a3b110591c696107ecb41fe6e9
Admin <yaveselyi@yandex.ru>
2023-01-31 00:18:32 +0800

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        String str = active ? "Обнаружена ошибка" : "Нет ошибок";
+        System.out.println(str);
+        System.out.println("№ " + status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error laptopLenovo = new Error();
+        Error laptopHp = new Error(true, 8, "Неисправна видеокарта");
+        Error laptopSamsung = new Error(false, 0, "Всё отлично работает");
+        laptopHp.printInfo();
+        System.out.println();
+        laptopSamsung.printInfo();
+    }
+}
