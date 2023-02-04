932d860fe0466765c6005c8a792f7bff81c3a368
Admin <yaveselyi@yandex.ru>
2023-02-04 23:15:05 +0800

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item object = new Item();
+        LocalDateTime objectClock = object.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String clock = objectClock.format(formatter);
+        System.out.println(clock);
+    }
+}
