da7d68e13647160afbff883c811ce3e383226958
Admin <yaveselyi@yandex.ru>
2023-02-09 22:46:02 +0800

    5. Tracker - хранилище [#396 #390047]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -6,7 +6,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TrackerTest  {
+public class TrackerTest {
 
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
