eafd1bcff24a7cc849263fb3cd822e77a04b771c
Admin <yaveselyi@yandex.ru>
2023-02-17 11:46:03 +0800

    10. toString [#310018]

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static int countCup(int count, int n) {
+        int res = 0;
+
+        return 0;
+    }
+}
+
---
+++ src/main/java/ru/job4j/poly/FileStore.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public class FileStore extends DbStore {
+    public void save(String value) {
+    }
+
+    public String[] load() {
+        return new String[0];
+    }
+}
