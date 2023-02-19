f4e3cbeff9eb145f5a28bcb187a58abadc7845a5
Admin <yaveselyi@yandex.ru>
2023-02-19 10:49:44 +0800

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749]

---
+++ src/main/java/ru/job4j/polymorph/TownCar.java
@@ -0,0 +1,38 @@
+package ru.job4j.polymorph;
+
+public class TownCar implements Vehicle, Fuel{
+    public void changeGear() {
+        System.out.println("Коробка передач автомат. Поставить селектор в режим D.");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("Нажатие на педаль газа механически открывает дроссельную заслонку");
+    }
+
+    @Override
+    public void steer() {
+        System.out.println("Стандартное рулевое управление");
+    }
+
+    @Override
+    public void refill() {
+        System.out.println("Заправить 100л бензина");
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("Стандартная тормозная система");
+    }
+
+    public static void main(String[] args) {
+        TownCar sportCar = new TownCar();
+        Vehicle vehicle = sportCar;
+        Fuel fuel = sportCar;
+        vehicle.changeGear();
+        vehicle.accelerate();
+        vehicle.steer();
+        vehicle.brake();
+        fuel.refill();
+    }
+}
