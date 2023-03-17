ecc3a154d908526a38dc5c3e39983dc30e474ba2
Admin <yaveselyi@yandex.ru>
2023-03-17 14:11:58 +0800

    12.1 Зависимость от System.out [#33568 #397164]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,9 +2,11 @@ package ru.job4j.tracker;
 
 public class StartUI {
     private final Output out;
+
     public StartUI(Output out) {
         this.out = out;
     }
+
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
@@ -21,6 +23,7 @@ public class StartUI {
             out.println(index + ". " + actions[index].name());
         }
     }
+
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
@@ -36,3 +39,4 @@ public class StartUI {
         };
         new StartUI(output).init(input, tracker, actions);
     }
+}
