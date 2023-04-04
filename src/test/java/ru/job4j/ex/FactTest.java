51e0f9a33001f151ecfd767163247e120b947693
Admin <yaveselyi@yandex.ru>
2023-04-04 10:06:54 +0800

    0.3. Тестирование исключений с junit [#219366 #403107]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class FactTest {
+class FactTest  {
 
     @Test
     public void whenException() {
